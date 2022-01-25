bf007e421b9e42385694ec0410969d0af0ee0af7
Talia Barrientos <talia.barrientosc@gmail.com>
2022-01-25 12:51:30 -0500

    Feat: Add changes

---
+++ src/main/java/com/taliabarrientos/demo/dto/SimplifiedUserResponse.java
@@ -0,0 +1,16 @@
+package com.taliabarrientos.demo.dto;
+
+import java.util.List;
+
+public class SimplifiedUserResponse {
+
+  private List<String> data;
+
+  public List<String> getData() {
+    return data;
+  }
+
+  public void setData(List<String> data) {
+    this.data = data;
+  }
+}
---
+++ src/main/java/com/taliabarrientos/demo/proxy/Support.java
@@ -0,0 +1,6 @@
+package com.taliabarrientos.demo.proxy;
+
+public class Support {
+  private String url;
+  private String text;
+}
